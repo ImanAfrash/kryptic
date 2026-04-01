7ad5bfa6a49625e7be780270b9368cfd328998fc
challenge <challenge@gmail.com>
2026-04-01 16:02:05 +0900

    feat: add sm screen setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
   theme: {
     // Custom screens (breakpoints)
     screens: {
+      'sm': '386px',
       'xs': '475px',
       'lg': '1024px',
       ...defaultTheme.screens, // Keep existing screens
